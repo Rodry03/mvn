1ae0a3569ea2636be2a0e364f6f2190ad4dd8b7d
Rodry03 <03.rodry.03@gmail.com>
2019-02-28 09:56:44 +0100

    Add files via upload

---
+++ maven-hello-world-master/my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        System.out.println("A NullPointerException is a drag...");
+
+        
+        System.out.println("... but thankfully, Checker has our back: http://checkerframework.org");
+    }
+}
